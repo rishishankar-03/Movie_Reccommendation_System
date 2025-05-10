ece38d5d509ae030d4274f4fdf6fa389990fb2e6
Rishi Shankar <rishishankar03@gmail.com>
2025-05-10 13:07:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+<<<<<<< HEAD
+import pickle
+import streamlit as st
+import requests
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+
+st.header('Movie Recommendation System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names = recommend(selected_movie)
+    for i in recommended_movie_names:
+=======
+import pickle
+import streamlit as st
+import requests
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    
+    for i in distances[1:6]:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+
+st.header('Movie Recommendation System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names = recommend(selected_movie)
+    for i in recommended_movie_names:
+>>>>>>> dd2dce3b5793e4e5619dbb6d10c569a53f1b0b0a
+        st.write(i)
